3ba0c9aa1b949fdf20c3c6a3ef9681a8f4765bb3
re-oh <dabrowskilucas2@gmail.com>
2024-08-23 12:18:34 +0200

    working connection panel

--- src/components/Connection/Connection.jsx
+++ src/components/Connection/Connection.jsx
@@ -8,6 +8,62 @@ const Connection = () => {
   const [userId, setUserId] = useState("");
   const [currentUsers, setCurrentUsers] = useState([]);
   const [conStatus, setConStatus] = useState("Not Connected");
+  const [socket, setSocket] = useState(undefined);
+  const [msg, setMsg] = useState("");
+
+  const startSocket = () => {
+    let ws = new WebSocket(
+      `http://127.0.0.1:8080/game/join?session_id=${lobby}&user_id=${userId}&username=${username}`
+    );
+
+    ws.addEventListener("open", (event) => {
+      console.log("Connected to WebSocket");
+      setConStatus("Connected");
+    });
+
+    ws.addEventListener("message", (event) => {
+      console.log("Message from server:", event.data);
+    });
+
+    ws.addEventListener("close", (event) => {
+      console.log("Disconnected from WebSocket");
+      setConStatus("Not Connected");
+      setSocket(undefined);
+    });
+
+    ws.addEventListener("error", (error) => {
+      console.error("WebSocket error:", error);
+    });
+
+    setSocket(ws);
+  };
+
+  const sendMessage = (msg) => {
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      socket.send(msg);
+    } else {
+      console.error("no connection");
+      return;
+    }
+  };
+
+  const createSession = () => {
+    fetch("http://127.0.0.1:8080/game/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        host_id: userId,
+        username: username,
+      }),
+    }).then((resp) => {
+      resp.json().then((text) => {
+        setLobby(text);
+        console.log(text);
+      });
+    });
+  };
 
   return (
     <div className="Connection">
@@ -37,11 +93,33 @@ const Connection = () => {
           setLobby(e.target.value);
         }}
       />
-
-      <button>Connect to session</button>
+      <button onClick={createSession}>Create Session</button>
+      <button
+        onClick={() => {
+          startSocket();
+          setConStatus("Attempting Connection");
+        }}
+      >
+        Connect to session
+      </button>
       <p>con status: {conStatus}</p>
 
+      <input
+        type="text"
+        value={msg}
+        onChange={(e) => {
+          setMsg(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          sendMessage(msg);
+        }}
+      >
+        Send Msg
+      </button>
       <p>Session Members:</p>
+
       <ul></ul>
     </div>
   );
